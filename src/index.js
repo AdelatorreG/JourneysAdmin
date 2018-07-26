341db26727ad507f3cba2cfee2ecce5dd95e1e85
AdelatorreG <yunechaos0929@gmail.com>
2018-07-26 11:01:38 -0700

    Cambios Viernes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 import Welcome from './components/welcomeforms';
 import Nav from './components/navbar';
 import Login from './components/loginforms';
 import Footer from './components/footer';
 import App from './components/App';
+var mongoose = require('mongoose');
 
 //ReactDOM.render(<Welcome/>, document.getElementById('wel'));
+
+//mongoose.connect("mongodb://localhost/journeys");
 
 //ReactDOM.render(<Nav />, document.getElementById('nav'));
 ReactDOM.render(<App />, document.getElementById('app'));
 //ReactDOM.render(<Nav />, document.getElementById('nav'));
 //ReactDOM.render(<Login />, document.getElementById('lg'));
 //ReactDOM.render(<Footer/>, document.getElementById('ftr'));
-registerServiceWorker();
 
 /*
-=======
->>>>>>> c1544250457fcc08fb5ad6e47331a670326c957f
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -33,17 +30,13 @@ app.set('port',process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.json());//cada vez que llega un dato verifica que sea un json 
 //routes
-<<<<<<< HEAD
 app.use('/api/airports',require('./routes/journeys.routes'));
-=======
 app.use('/api/journeys',require('./routes/journeys.routes'));
 
 
->>>>>>> c1544250457fcc08fb5ad6e47331a670326c957f
 //static files
 app.use(express.static(path.join(__dirname,'public')));
 //starting server
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`);
 });*/
-registerServiceWorker();
